74c317cca4ee666fa82f6b0425df22a800eba2d2
Sherry868 <m.khurram@gmail.com>
2024-10-02 21:31:36 +0500

    home page in progress

--- resources/views/layouts/frontend/footer.blade.php
+++ resources/views/layouts/frontend/footer.blade.php
@@ -1,6 +1,58 @@
- <!-- Footer Section -->
- <footer class="bg-dark text-white py-3">
-     <div class="container text-center">
-         <p>&copy; 2024 My Website. All Rights Reserved.</p>
-     </div>
- </footer>
+<footer class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <!-- Column 1: Title, Description, and Social Links -->
+            <div class="col-md-3 mb-4">
+                <h5>My Website</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.</p>
+                <div class="social-links mt-3">
+                    <a href="#" class="text-white mr-3"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="text-white mr-3"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="text-white mr-3"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+
+            <!-- Column 2: Explore Links -->
+            <div class="col-md-3 mb-4">
+                <h5>Explore</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Home</a></li>
+                    <li><a href="#" class="text-white">Services</a></li>
+                    <li><a href="#" class="text-white">Products</a></li>
+                    <li><a href="#" class="text-white">Contact</a></li>
+                </ul>
+            </div>
+
+            <!-- Column 3: External Links -->
+            <div class="col-md-3 mb-4">
+                <h5>External Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Blog</a></li>
+                    <li><a href="#" class="text-white">Partners</a></li>
+                    <li><a href="#" class="text-white">Affiliates</a></li>
+                    <li><a href="#" class="text-white">Careers</a></li>
+                </ul>
+            </div>
+
+            <!-- Column 4: About Us Links -->
+            <div class="col-md-3 mb-4">
+                <h5>About Us</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Our Story</a></li>
+                    <li><a href="#" class="text-white">Team</a></li>
+                    <li><a href="#" class="text-white">Privacy Policy</a></li>
+                    <li><a href="#" class="text-white">Terms of Service</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Footer Bottom Section -->
+        <div class="text-center mt-4">
+            <p>&copy; 2024 My Website. All Rights Reserved.</p>
+        </div>
+    </div>
+</footer>
+
+<!-- Include Font Awesome for social icons -->
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,18 +8,61 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-7">
-                    <h2 class="hero-title">FIND A SKILLED SQUAD IN 5 MINUTES!</h2>
-                    <p class="hero-description">Use our Data Driven insights to match your skill, language, and play style
+                    <h2 class="hero-title mb-4">FIND A SKILLED SQUAD IN <span>5 MINUTES!</span></h2>
+                    <p class="hero-description mb-4">Use our <span> Data Driven</span></br>
+                        insights to match your <span>skill</span>,</br><span>language</span>, and <span>play style</span>
                         now!</p>
                     <a href="#" class="btn btn-outline-dark mt-4">JOIN OUR DISCORD</a>
                 </div>
             </div>
         </div>
     </section>
-
     <!-- Main Content -->
-    <div class="container my-5">
-        <h2>About Us</h2>
-        <p>Some content about your website...</p>
+    <div class="container-lg d-flex justify-content-center align-items-center" style="height: 50vh">
+        <div class="row w-200">
+            <div class="col-md-4 d-flex justify-content-center align-items-left">
+                <img src="{{ asset('frontend/images/image1.png') }}" class="img-fluid" alt="Hero Image" />
+            </div>
+            <div class="col-md-8 d-flex flex-column justify-content-center align-items-right">
+                <h2 class="hero-title mb-4">FIND <span>YOUR</span> COMMUNITY </h2>
+                <p class="hero-description mb-4">10,000 of the world’s best PUBG players 200+
+                    skilled squads per day 100+ professional PUBG Gamers 20+ Open Community Tier Tournaments
+                </p>
+                <p class="hero-description mb-4">
+                    NOW! Expanding to new Esports Titles</p>
+            </div>
+        </div>
+    </div>
+    <!-- Main Content -->
+    <div class="container-lg d-flex justify-content-center align-items-center" style="height: 50vh">
+        <div class="row w-200">
+            <div class="col-md-8 d-flex flex-column justify-content-center align-items-right">
+                <h2 class="hero-title mb-4">FIND <span>YOUR</span> COMMUNITY </h2>
+                <p class="hero-description mb-4">10,000 of the world’s best PUBG players 200+
+                    skilled squads per day 100+ professional PUBG Gamers 20+ Open Community Tier Tournaments
+                </p>
+                <p class="hero-description mb-4">
+                    NOW! Expanding to new Esports Titles</p>
+            </div>
+        </div>
+        <div class="col-md-4 d-flex justify-content-center align-items-left">
+            <img src="{{ asset('frontend/images/image1.png') }}" class="img-fluid" alt="Hero Image" />
+        </div>
+    </div>
+    <!-- Main Content -->
+    <div class="container-lg d-flex justify-content-center align-items-center" style="height: 50vh">
+        <div class="row w-200">
+            <div class="col-md-4 d-flex justify-content-center align-items-left">
+                <img src="{{ asset('frontend/images/image1.png') }}" class="img-fluid" alt="Hero Image" />
+            </div>
+            <div class="col-md-8 d-flex flex-column justify-content-center align-items-right">
+                <h2 class="hero-title mb-4">FIND <span>YOUR</span> COMMUNITY </h2>
+                <p class="hero-description mb-4">10,000 of the world’s best PUBG players 200+
+                    skilled squads per day 100+ professional PUBG Gamers 20+ Open Community Tier Tournaments
+                </p>
+                <p class="hero-description mb-4">
+                    NOW! Expanding to new Esports Titles</p>
+            </div>
+        </div>
     </div>
 @endsection
